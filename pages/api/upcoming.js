1be91af8636616369a31c4be01a8b45f274ba7c3
Aditia Prasetian <aditiapras97@gmail.com>
2023-08-03 00:24:00 +0700

    Update upcoming.js

--- pages/api/upcoming.js
+++ pages/api/upcoming.js
@@ -1,6 +1,9 @@
 export default async function handler(req, res) {
-  const getData = await fetch(process.env.NEXT_PUBLIC_BASE_URL).then((r) =>
-    r.json()
-  );
+  const getData = await fetch(process.env.NEXT_PUBLIC_BASE_URL, {
+    headers: {
+      "X-Base-URL": process.env.NEXT_PUBLIC_BASE,
+      "X-API-KEY": process.env.NEXT_PUBLIC_KEY,
+    },
+  }).then((r) => r.json());
   res.status(200).json({ getData });
 }
