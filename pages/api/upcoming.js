05203d356d6dfc421396e431cf297ca37b4b5d8b
aditiaprasdev <aditiapras97@gmail.com>
2023-08-06 00:35:32 +0700

    Update upcoming.js

--- pages/api/upcoming.js
+++ pages/api/upcoming.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const getData = await fetch(process.env.NEXT_PUBLIC_BASE_URL, {
     headers: {
       "X-Base-URL": process.env.NEXT_PUBLIC_BASE,
-      "X-API-KEY": process.env.NEXT_PUBLIC_KEY,
+      "X-API-Key": process.env.NEXT_PUBLIC_KEY,
     },
   }).then((r) => r.json());
   res.status(200).json({ getData });
